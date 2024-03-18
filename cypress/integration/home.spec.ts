c7759149aedf06aca547b698cbb2169e652a9dba
ledouxm <martin.ledoux.pro@gmail.com>
2024-03-18 12:58:14 +0100

    refactor: replace previous template

--- cypress/integration/home.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("Home page", () => {
-  it("should render the main page", () => {
-    cy.visit("http://localhost:3000/");
-    cy.title().should("equal", "Template | beta.gouv.fr");
-    cy.get("h1").should("contain", "Template");
-  });
-});
