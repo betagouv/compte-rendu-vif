0b6c533ac3fc73269decb979b7c397061f2891a0
ledouxm <martin.ledoux.pro@gmail.com>
2024-04-02 08:09:02 +0200

    refactor: generate panda ds using regex instead of ast

--- packages/frontend/scripts/generatePandaDS.ts
+++ packages/frontend/scripts/generatePandaDS.ts
@@ -1,18 +1,21 @@
-import { SemanticTokens, defineConfig } from "@pandacss/dev";
 import fs from "fs/promises";
-import * as cssTree from "css-tree";
+import { defineConfig } from "@pandacss/dev";
+import { parseBreakpoints, parseSpacings, parseTypography } from "./parseDsfrFRVariable";
 
-import { parseTypography, parseSpacings, parseBreakpoints } from "./parseDsfrFRVariable";
-
-const parseDS = async () => {
-  const { textStyles, fontWeights } = parseTypography();
-
+export const generatePandaDSRegex = async () => {
   let dsfrContent = await fs.readFile("./public/dsfr/dsfr.min.css", "utf-8");
   dsfrContent = dsfrContent.slice('@charset "UTF-8";'.length);
 
-  const variables = listEveryVariable(dsfrContent);
-  const everyVariable = { ...variables.light, ...variables.dark };
-  const semanticColorTokens = getSemanticTokens(variables);
+  const parser = new Parser(dsfrContent);
+
+  const fontFaces = parser.extractFontFaces();
+  const { light, dark, rootBlock, darkRootBlock } = parser.extractLightAndDarkVariables();
+  const semanticTokens = getSemanticTokens({ light, dark });
+
+  const replaced = parser.replaceVariableNames(semanticTokens);
+  const newContent = [rootBlock, darkRootBlock, ...fontFaces, "@layer dsfr {", replaced, "}"].join("\n");
+
+  const { textStyles, fontWeights } = parseTypography();
 
   const tokens = defineConfig({
     theme: {
@@ -22,147 +25,121 @@ const parseDS = async () => {
           fontWeights,
         },
         semanticTokens: {
-          colors: semanticColorTokens,
+          colors: semanticTokens,
         },
         breakpoints: parseBreakpoints(),
         textStyles,
       },
     },
   });
-
   await fs.writeFile("./dsfr-tokens.json", JSON.stringify(tokens, null, 2));
-
-  const newRootBlock = removeDSFRVariables(dsfrContent, everyVariable);
-  dsfrContent = getNextBlock({ content: dsfrContent, blockName: ":root[data-fr-theme=dark]", remove: true })!.rest;
-  dsfrContent = getNextBlock({ content: dsfrContent, blockName: ":root", remove: true })!.rest;
-
-  dsfrContent = replaceDSFRVariablesWithPanda(dsfrContent, everyVariable);
-  const fontFaces = [] as { start: number; end: number; block: string }[];
-
-  // find and remove all font-faces
-  while (dsfrContent.includes("@font-face")) {
-    const { rest, ...item } = getNextBlock({ content: dsfrContent, blockName: "@font-face", remove: true })!;
-    fontFaces.push(item);
-
-    dsfrContent = rest;
-  }
-
-  const newContent = [newRootBlock, ...fontFaces.map((f) => f.block), "@layer dsfr {", dsfrContent, "}"].join("\n");
-
   await fs.writeFile("./public/dsfr/dsfr-patched.css", newContent);
 };
 
+const getSemanticTokens = ({ light, dark }: { light: Record<string, string>; dark: Record<string, string> }) => {
+  const grouped = {} as Record<string, { value: { base: string; _dark: string } }>;
+
+  for (const [name, value] of Object.entries(light)) {
+    const isPrimitive = !value.includes("var(");
+
+    const atomicVarName = isPrimitive ? name : value.replace(/var\((.*)\)/, (_, varName) => varName);
+
+    const lightValue = light[atomicVarName];
+    const darkValue = dark[atomicVarName];
+
+    grouped[name.replace("--", "")] = { value: { base: lightValue, _dark: darkValue } };
+  }
+
+  return grouped;
+};
+
+class Parser {
+  constructor(public content: string) {}
+
+  getBlockAndRemove = (blockName: string) => {
+    const { block, rest } = getNextBlock({ content: this.content, blockName, remove: true })!;
+    this.content = rest;
+    return block;
+  };
+
+  extractFontFaces = () => {
+    const fontFaces = [] as string[];
+
+    while (this.content.includes("@font-face")) {
+      const block = this.getBlockAndRemove("@font-face")!;
+      fontFaces.push(block);
+    }
+
+    return fontFaces;
+  };
+
+  extractLightAndDarkVariables = () => {
+    const lightVariablesRaw = this.extractVariables(this.getBlockAndRemove(":root")!);
+    const darkVariablesRaw = this.extractVariables(this.getBlockAndRemove(":root[data-fr-theme=dark]")!);
+
+    const lightVariables = this.filterColorVariables(lightVariablesRaw);
+    const darkVariables = this.filterColorVariables(darkVariablesRaw);
+
+    const rootBlock = `:root{${Object.entries(lightVariables.untouched)
+      .map(([name, value]) => `${name}:${value};`)
+      .join("")}}`;
+    const darkRootBlock = `:root[data-fr-theme=dark]{${Object.entries(darkVariables.untouched)
+      .map(([name, value]) => `${name}:${value};`)
+      .join("")}}`;
+
+    return { light: lightVariables.colors, dark: darkVariables.colors, rootBlock, darkRootBlock };
+  };
+
+  extractVariables = (block: string) => {
+    const variables = {} as Record<string, string>;
+
+    let match;
+    while ((match = variableDeclRegex.exec(block))) {
+      const [variableDecl] = match;
+      const [name, value] = variableDecl.split(":");
+
+      variables[name] = value.replace(";", "");
+    }
+
+    return variables;
+  };
+
+  filterColorVariables = (variables: Record<string, string>) => {
+    return Object.entries(variables).reduce(
+      (acc, [name, value]) => {
+        const flatValue = value.includes("var(")
+          ? value.replace(/var\((.*)\)/, (_, varName) => variables[varName])
+          : value;
+
+        if (colorValueRegex.test(flatValue)) {
+          acc.colors[name] = value;
+        } else {
+          acc.untouched[name] = value;
+        }
+
+        return acc;
+      },
+      { untouched: {}, colors: {} } as { untouched: Record<string, string>; colors: Record<string, string> },
+    );
+  };
+
+  replaceVariableNames = (semanticTokens: Record<string, any>) => {
+    const variableNames = Object.keys(semanticTokens);
+
+    for (const variableName of variableNames) {
+      const pandaVariableName = "--colors-" + variableName;
+      const stringToReplace = `var(--${variableName})`;
+      while (this.content.includes(stringToReplace)) {
+        this.content = this.content.replace(stringToReplace, `var(${pandaVariableName})`);
+      }
+    }
+
+    return this.content;
+  };
+}
+
 const colorValueRegex = /#[0-9a-fA-F]{3,6}/;
-
-const listEveryVariable = (content: string) => {
-  const { block: rootBlock } = getNextBlock({ content, blockName: ":root" })!;
-  const { block: rootBlockDark } = getNextBlock({ content, blockName: ":root[data-fr-theme=dark]" })!;
-
-  const variables = {} as Record<string, string>;
-  const variablesDark = {} as Record<string, string>;
-
-  let ast = cssTree.parse(rootBlock, { context: "stylesheet" });
-
-  cssTree.walk(ast, {
-    visit: "Declaration",
-    enter(node: cssTree.Declaration) {
-      if (node.property.startsWith("--")) {
-        variables[node.property as string] = (node.value as cssTree.Raw).value;
-      }
-    },
-  });
-
-  ast = cssTree.parse(rootBlockDark, { context: "stylesheet" });
-  cssTree.walk(ast, {
-    visit: "Declaration",
-    enter(node: cssTree.Declaration) {
-      if (node.property.startsWith("--")) {
-        variablesDark[node.property as string] = (node.value as cssTree.Raw).value;
-      }
-    },
-  });
-
-  return { light: filterColorVariables(variables), dark: filterColorVariables(variablesDark) };
-};
-
-const filterColorVariables = (variables: Record<string, string>) => {
-  return Object.entries(variables).reduce(
-    (acc, [key, value]) => {
-      const flatValue = value.includes("var(")
-        ? value.replace(/var\((.*)\)/, (_, varName) => variables[varName])
-        : value;
-
-      if (!colorValueRegex.test(flatValue)) return acc;
-
-      acc[key] = value;
-      return acc;
-    },
-    {} as Record<string, string>,
-  );
-};
-
-const getSemanticTokens = (variables: ReturnType<typeof listEveryVariable>) => {
-  const colorDecisions = Object.entries(variables.light).reduce(
-    (acc, [key, value]) => {
-      // TODO: check if a semantic token can reference another one
-      // const darkValue = variables.dark[key];
-      // const baseValue = isPrimitive ? value : `{colors.${value.slice("var(--".length, -1)}}`;
-      const isPrimitive = !value.includes("var(");
-
-      const darkValue = variables.dark[isPrimitive ? key : value.slice("var(".length, -1)];
-      const baseValue = isPrimitive ? value : value.replace(/var\((.*)\)/, (_, varName) => variables.light[varName]);
-
-      const cleanKey = key.replace("--", "");
-      acc[cleanKey] = { value: { base: baseValue, _dark: darkValue } };
-
-      return acc;
-    },
-    {} as NonNullable<SemanticTokens["colors"]>,
-  );
-
-  return colorDecisions;
-};
-
-const replaceDSFRVariablesWithPanda = (content: string, variables: Record<string, any>) => {
-  const ast = cssTree.parse(content, { context: "stylesheet" });
-
-  cssTree.walk(ast, {
-    visit: "Function",
-    enter(node: cssTree.FunctionNode) {
-      if (node.name === "var") {
-        const child = node.children.first as cssTree.Identifier;
-
-        if (!child) return;
-        if (!variables[child.name]) return;
-
-        child.name = formatVariableName(child.name);
-      }
-    },
-  });
-
-  return cssTree.generate(ast);
-};
-
-const formatVariableName = (name: string) => "--colors-" + name.slice(2);
-
-const removeDSFRVariables = (content: string, variables: Record<string, any>) => {
-  const { block: rootBlock, end } = getNextBlock({ content, blockName: ":root" })!;
-  content = content.slice(end);
-
-  const rootAst = cssTree.parse(rootBlock, { context: "stylesheet" });
-  cssTree.walk(rootAst, {
-    visit: "Declaration",
-    enter(node: cssTree.Declaration, item, list) {
-      if (variables[node.property as string]) {
-        list.remove(item);
-      }
-    },
-  });
-
-  const newRootBlock = cssTree.generate(rootAst);
-
-  return newRootBlock;
-};
+const variableDeclRegex = /--[^:]+:[^;]+;/g;
 
 const getNextBlock = ({ content, blockName, remove }: { content: string; blockName: string; remove?: boolean }) => {
   const start = content.indexOf(blockName + "{");
@@ -174,7 +151,7 @@ const getNextBlock = ({ content, blockName, remove }: { content: string; blockNa
     content = content.replace(block, "");
   }
 
-  return { start, end, block, rest: content };
+  return { block, rest: content };
 };
 
-parseDS();
+generatePandaDSRegex();
