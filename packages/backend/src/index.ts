4d0891913035a4024fd5538d3a90eddad0f89a7d
ledouxm <martin.ledoux.pro@gmail.com>
2024-04-08 16:12:51 +0200

    wip: add bun

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -4,9 +4,8 @@ import { onHmr, registerViteHmrServerRestart } from "./hmr";
 import { db, migrateUsersDb } from "./db/db";
 import { fastifyTRPCPlugin, FastifyTRPCPluginOptions } from "@trpc/server/adapters/fastify";
 import fastify from "fastify";
-import { appRouter, AppRouter } from "./router";
-import { createContext } from "./trpc";
-import cors from "@fastify/cors";
+import { elysia } from "./router";
+import { cors } from "@elysiajs/cors";
 import { ENV } from "./envVars";
 import { AppError } from "./features/errors";
 
@@ -16,32 +15,30 @@ const start = async () => {
 
   await migrateUsersDb();
 
-  const fastifyInstance = fastify({ maxParamLength: 5000 });
+  // fastifyInstance.register(fastifyTRPCPlugin, {
+  //   prefix: "/api",
+  //   trpcOptions: {
+  //     router: appRouter,
+  //     createContext,
+  //     onError({ path, error }) {
+  //       console.error(`[${path}] ${error.message}`);
+  //       if (!(error instanceof AppError)) {
+  //         error.message = "Une erreur s'est produite. Veuillez réessayer plus tard.";
+  //         console.error(error.stack);
+  //       }
+  //     },
+  //   } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
+  // });
 
-  fastifyInstance.register(cors, {
-    origin: "*",
+  elysia.use(cors());
+  elysia.listen(ENV.HTTP_PORT, () => {
+    console.log(`Listening on port ${ENV.HTTP_PORT}`);
   });
-
-  fastifyInstance.register(fastifyTRPCPlugin, {
-    prefix: "/api",
-    trpcOptions: {
-      router: appRouter,
-      createContext,
-      onError({ path, error }) {
-        console.error(`[${path}] ${error.message}`);
-        if (!(error instanceof AppError)) {
-          error.message = "Une erreur s'est produite. Veuillez réessayer plus tard.";
-          console.error(error.stack);
-        }
-      },
-    } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
-  });
-
-  await fastifyInstance.listen({ port: ENV.HTTP_PORT });
+  // await fastifyInstance.listen({ port: ENV.HTTP_PORT });
 
   onHmr(async () => {
     // server.close();
-    await fastifyInstance.close();
+    await elysia.stop();
   });
 };
 
