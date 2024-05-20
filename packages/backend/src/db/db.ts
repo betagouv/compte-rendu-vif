79f91235e638bdfc68b0e86c0bec840e1ccd7764
ledouxm <martin.ledoux.pro@gmail.com>
2024-05-20 10:16:34 +0200

    ci: log env

--- packages/backend/src/db/db.ts
+++ packages/backend/src/db/db.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@cr-vif/electric-client/backend";
 import { ENV } from "../envVars";
-
+console.log(ENV);
 export const db = new PrismaClient({ datasources: { db: { url: ENV.DATABASE_URL } } });
 
 export const cleanUpDb = async () => {
