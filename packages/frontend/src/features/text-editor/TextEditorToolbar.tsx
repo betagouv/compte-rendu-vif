01b4ad3537ac33dc6ac292834f38f62452d4a900
ledouxm <martin.ledoux.pro@gmail.com>
2024-08-13 17:36:55 +0200

    fix: new colors

--- packages/frontend/src/features/text-editor/TextEditorToolbar.tsx
+++ packages/frontend/src/features/text-editor/TextEditorToolbar.tsx
@@ -105,18 +105,13 @@ const ColorInput = () => {
           <Stack p="8px">
             <Stack gap="8px" flexDir="row" justifyContent="space-around" w="100%">
               <ColorButton value={currentValue} color="black" />
-              <ColorButton value={currentValue} color="#c9191e" />
-              <ColorButton value={currentValue} color="#666666" />
+              <ColorButton value={currentValue} color="#e1000f" />
+              <ColorButton value={currentValue} color="#000091" />
             </Stack>
             <Stack gap="8px" flexDir="row" justifyContent="space-around" w="100%">
-              <ColorButton value={currentValue} color="#cecece" />
-              <ColorButton value={currentValue} color="#0078f3" />
-              <ColorButton value={currentValue} color="#1f8d49" />
-            </Stack>
-            <Stack gap="8px" flexDir="row" justifyContent="space-around" w="100%">
-              <ColorButton value={currentValue} color="#68a532" />
-              <ColorButton value={currentValue} color="#e4794a" />
-              <ColorButton value={currentValue} color="#272747" />
+              <ColorButton value={currentValue} color="#118d49" />
+              <ColorButton value={currentValue} color="#d64d00" />
+              <div/>
             </Stack>
           </Stack>
         </Popover.Content>
