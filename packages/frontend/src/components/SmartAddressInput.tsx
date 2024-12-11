523669c6abdd14c4e1b1a7b22211d1949918a03c
ledouxm <martin.ledoux.pro@gmail.com>
2024-12-11 16:53:14 +0100

    feat: add loading badge and fix some styles

--- packages/frontend/src/components/SmartAddressInput.tsx
+++ packages/frontend/src/components/SmartAddressInput.tsx
@@ -1,16 +1,16 @@
-import Input from "@codegouvfr/react-dsfr/Input";
 import { css } from "#styled-system/css";
-import { Control, useController, useFormContext, useWatch } from "react-hook-form";
-import { Report } from "../db/AppSchema";
-import { useIsFormDisabled } from "../features/DisabledContext";
+import { Flex, Stack, styled } from "#styled-system/jsx";
+import { fr } from "@codegouvfr/react-dsfr";
+import Badge from "@codegouvfr/react-dsfr/Badge";
+import Input from "@codegouvfr/react-dsfr/Input";
 import { useQuery } from "@tanstack/react-query";
-import { searchAddress } from "../features/address";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useFormContext, useWatch } from "react-hook-form";
 import { useDebounce } from "react-use";
-import { Popover } from "./Popover";
-import { styled } from "#styled-system/jsx";
+import { Report } from "../db/AppSchema";
+import { AddressResult, searchAddress } from "../features/address";
+import { useIsFormDisabled } from "../features/DisabledContext";
 import { Combobox } from "./Combobox";
-import Button from "@codegouvfr/react-dsfr/Button";
 
 export const SmartAddressInput = () => {
   const form = useFormContext<Report>();
@@ -23,21 +23,24 @@ export const SmartAddressInput = () => {
 
   useDebounce(() => setDebouncedAddress(applicantAddress), 500, [applicantAddress]);
 
+  const isEnabled = !isFormDisabled && debouncedAddress && debouncedAddress.length > 4 && !isFrozen;
+
   const addressQuery = useQuery({
     queryKey: ["address", debouncedAddress],
     queryFn: () => searchAddress(debouncedAddress!),
-    enabled: !!debouncedAddress && !isFormDisabled && debouncedAddress.length > 4 && !isFrozen,
+    enabled: !!isEnabled,
   });
 
+  const isLoading = addressQuery.isLoading && isEnabled;
   const suggestions = addressQuery.data;
 
   return (
-    <styled.div mb="28px">
+    <Stack mb="28px">
       <Combobox.Root
         disabled={isFormDisabled}
         selectionBehavior="replace"
-        itemToString={(item) => (item as string) ?? ""}
-        itemToValue={(item) => (item as string) ?? ""}
+        itemToString={(item) => (item as AddressResult).address ?? ""}
+        itemToValue={(item) => (item as AddressResult).label ?? ""}
         items={suggestions ?? []}
         value={applicantAddress ? [applicantAddress.toString()] : undefined}
         inputValue={applicantAddress ?? ""}
@@ -46,25 +49,27 @@ export const SmartAddressInput = () => {
           setIsFrozen(false);
         }}
         onValueChange={(e) => {
-          form.setValue("applicantAddress", e.items?.[0] as string);
+          form.setValue("applicantAddress", (e.items?.[0] as AddressResult)?.address ?? "");
+          form.setValue("zipCode", (e.items?.[0] as AddressResult)?.zipCode ?? "");
+          form.setValue("city", (e.items?.[0] as AddressResult)?.city ?? "");
           setIsFrozen(true);
         }}
       >
         <Combobox.Control>
-          <Combobox.Input asChild placeholder="Sélectionner un service instructeur">
-            <ProxyInput disabled={isFormDisabled} />
+          <Combobox.Input asChild placeholder="">
+            <ProxyInput isLoading={isLoading} disabled={isFormDisabled} />
           </Combobox.Input>
           {/* <Combobox.Trigger asChild top="unset !important" bottom="28px">
             <Button iconId="ri-arrow-down-line" aria-label="open" priority="tertiary no outline" size="small"></Button>
           </Combobox.Trigger> */}
         </Combobox.Control>
         <Combobox.Positioner>
-          <Combobox.Content maxH="400px" overflow="auto">
+          <Combobox.Content maxH="400px" bgColor="background-contrast-grey" overflow="auto">
             <Combobox.ItemGroup id="service-instructeur">
               {suggestions?.length
-                ? suggestions.map((item: string) => (
-                    <Combobox.Item key={item} item={item}>
-                      <Combobox.ItemText>{item}</Combobox.ItemText>
+                ? suggestions.map((item: AddressResult) => (
+                    <Combobox.Item key={item.label} item={item}>
+                      <Combobox.ItemText>{item.label}</Combobox.ItemText>
                     </Combobox.Item>
                   ))
                 : null}
@@ -72,10 +77,54 @@ export const SmartAddressInput = () => {
           </Combobox.Content>
         </Combobox.Positioner>
       </Combobox.Root>
-    </styled.div>
+      {isLoading ? (
+        <styled.div hideFrom="lg" mt="8px">
+          <LoadingBadge />
+        </styled.div>
+      ) : null}
+    </Stack>
   );
 };
 
-const ProxyInput = ({ disabled, ...props }: any) => {
-  return <Input label="Adresse" disabled={disabled} nativeInputProps={{ ...props, autoComplete: "new-password" }} />;
+const ProxyInput = ({ disabled, isLoading, ...props }: any) => {
+  return (
+    <Input
+      label={
+        <Flex flexDir="row" alignItems="center">
+          <styled.span mr="12px">Adresse (numéro, voie)</styled.span>
+          {isLoading ? (
+            <styled.div hideBelow="lg">
+              <LoadingBadge />
+            </styled.div>
+          ) : null}
+        </Flex>
+      }
+      disabled={disabled}
+      nativeInputProps={{ ...props, autoComplete: "new-password" }}
+    />
+  );
+};
+
+const LoadingBadge = () => {
+  return (
+    <Badge
+      className={css({
+        display: "flex",
+        flexDir: "row",
+        alignItems: "center",
+        fontWeight: "normal",
+      })}
+      severity="info"
+      noIcon
+    >
+      <styled.i
+        className={fr.cx("fr-icon-refresh-line", "fr-icon--sm")}
+        _before={{
+          verticalAlign: "middle",
+          mr: "4px",
+        }}
+      ></styled.i>
+      Recherche en cours
+    </Badge>
+  );
 };
--- packages/frontend/src/features/address.tsx
+++ packages/frontend/src/features/address.tsx
@@ -2,7 +2,21 @@ export const searchAddress = async (query: string) => {
   const response = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${query}&limit=10`);
   const data = await response.json();
 
-  return data.features.map((feature: AddressFeature) => feature.properties.label);
+  return data.features.map((feature: AddressFeature, index: number) => ({
+    index,
+    address: feature.properties.name,
+    zipCode: feature.properties.postcode,
+    city: feature.properties.city,
+    label: feature.properties.label,
+  }));
+};
+
+export type AddressResult = {
+  address: string;
+  zipCode: string;
+  city: string;
+  label: string;
+  index: number;
 };
 
 export interface AddressFeature {
